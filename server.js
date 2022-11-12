d96f6f5980117c6f53062ac13e96fc5b390e1ddc
nguyenthanhco <conguyen.270601@gmail.com>
2022-11-12 16:33:38 +0700

    init server

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const { Server } = require('socket.io')
+const cors = require('cors')
+const port = 3001;
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+const server = http.createServer(app)
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+        method: ['GET', 'POST'],
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log(socket.id)
+    socket.on("send-message", (data) => {
+        socket.broadcast.emit("receive-message", data)
+        console.log(data)
+    })
+    socket.on('disconnect', (data) => {
+        console.log(socket.id)
+    })
+})
+
+server.listen(port, () => {
+    console.log('http://localhost:%d', port);
+})
